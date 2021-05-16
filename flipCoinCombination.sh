6fe7041f809344ac2c2c4999da68deeb4fcaff74
Srikar <srikarsamanchi1999@gmail.com>
2021-05-16 16:13:21 +0530

    Added: winning percentage

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,10 +1,18 @@
 #!/bin/bash -x
+declare -A dict
+dict[H]=0
+dict[T]=0
 
+for ((var=1; var<=10; var++))
+do
 toss=$((RANDOM%2))
 
 if [ $toss -eq 0 ]
 then
-echo Heads
+((dict[H]++))
 else
-echo Tails
+((dict[T]++))
 fi
+done
+echo percentage of heads $((${dict[H]}*10))
+echo percentage of tails $((${dict[T]}*10))
