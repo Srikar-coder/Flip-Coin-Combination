338b0c0250deea065b9f5b137b7c7968c507e1ac
Srikar <srikarsamanchi1999@gmail.com>
2021-05-16 16:34:44 +0530

    Added: Doublet combination

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,18 +1,29 @@
 #!/bin/bash -x
 declare -A dict
-dict[H]=0
-dict[T]=0
+dict[HH]=0
+dict[TT]=0
+dict[HT]=0
+dict[TH]=0
 
 for ((var=1; var<=10; var++))
 do
 toss=$((RANDOM%2))
-
-if [ $toss -eq 0 ]
+toss2=$((RANDOM%2))
+if [[ $toss -eq 0 && $toss2 -eq 0 ]]
 then
-((dict[H]++))
+((dict[HH]++))
+elif [[ $toss -eq 1 && $toss2 -eq 0 ]]
+then
+((dict[TH]++))
+elif [[ $toss -eq 0 && $toss2 -eq 1 ]]
+then
+((dict[HT]++))
 else
-((dict[T]++))
+((dict[TT]++))
 fi
 done
-echo percentage of heads $((${dict[H]}*10))
-echo percentage of tails $((${dict[T]}*10))
+echo percentage of two heads $((${dict[HH]}*10))
+echo percentage of two tails $((${dict[TT]}*10))
+
+echo percentage of heads tails $((${dict[HT]}*10))
+echo percentage of tails heads $((${dict[TH]}*10))
