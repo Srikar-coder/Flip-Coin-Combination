940d5c47b80d0076ae75aac61143c233d3d9d7c0
Srikar <srikarsamanchi1999@gmail.com>
2021-05-16 17:05:25 +0530

    Added: Triplet combination percentage

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,29 +1,50 @@
 #!/bin/bash -x
 declare -A dict
-dict[HH]=0
-dict[TT]=0
-dict[HT]=0
-dict[TH]=0
+dict[HHH]=0
+dict[HHT]=0
+dict[HTH]=0
+dict[HTT]=0
+dict[TTT]=0
+dict[TTH]=0
+dict[THT]=0
+dict[THH]=0
 
 for ((var=1; var<=10; var++))
 do
 toss=$((RANDOM%2))
 toss2=$((RANDOM%2))
-if [[ $toss -eq 0 && $toss2 -eq 0 ]]
+toss3=$((RANDOM%2))
+if [[ $toss -eq 0 && $toss2 -eq 0 && $toss3 -eq 0 ]]
 then
-((dict[HH]++))
-elif [[ $toss -eq 1 && $toss2 -eq 0 ]]
+((dict[HHH]++))
+elif [[ $toss -eq 0 && $toss2 -eq 0 && $toss3 -eq 1 ]]
 then
-((dict[TH]++))
-elif [[ $toss -eq 0 && $toss2 -eq 1 ]]
+((dict[HHT]++))
+elif [[ $toss -eq 0 && $toss2 -eq 1 && $toss3 -eq 0 ]]
 then
-((dict[HT]++))
-else
-((dict[TT]++))
+((dict[HTH]++))
+elif [[ $toss -eq 0 && $toss2 -eq 1 && $toss3 -eq 1 ]]
+then
+((dict[HTT]++))
+elif [[ $toss -eq 1 && $toss2 -eq 1 && $toss3 -eq 1 ]]
+then
+((dict[TTT]++))
+elif [[ $toss -eq 1 && $toss2 -eq 1 && $toss3 -eq 0 ]]
+then
+((dict[TTH]++))
+elif [[ $toss -eq 1 && $toss2 -eq 0 && $toss3 -eq 1 ]]
+then
+((dict[THT]++))
+elif [[ $toss -eq 1 && $toss2 -eq 0 && $toss3 -eq 0 ]]
+then
+((dict[THH]++))
 fi
 done
-echo percentage of two heads $((${dict[HH]}*10))
-echo percentage of two tails $((${dict[TT]}*10))
-
-echo percentage of heads tails $((${dict[HT]}*10))
-echo percentage of tails heads $((${dict[TH]}*10))
+echo  HHH $((${dict[HHH]}*10))
+echo  HHT $((${dict[HHT]}*10))
+echo  HTH $((${dict[HTH]}*10))
+echo  HTT $((${dict[HTT]}*10))
+echo  TTT $((${dict[TTT]}*10))
+echo  TTH $((${dict[TTH]}*10))
+echo  THT $((${dict[THT]}*10))
+echo  THH $((${dict[THH]}*10))
